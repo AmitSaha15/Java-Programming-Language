6d304f0a2a7ebf4147578096386c1c85decab208
Amit Saha <asaha.bwn2000@gmail.com>
2023-06-26 20:37:25 +0530

    learning java

---
+++ areaOfCircle.java
@@ -0,0 +1,10 @@
+// finding area of circle
+import java.util.*;
+public class areaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+    }
+}
