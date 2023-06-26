964bbbdfa6cf4a25e966c1773509f03578a54e8e
Amit Saha <asaha.bwn2000@gmail.com>
2023-06-26 19:27:55 +0530

    taking input

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,10 +1,22 @@
+import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         // code to calculate sum
         int a = 10;
         int b = 5;
         int sum = a+b;
-        System.out.println(sum);
+        // System.out.println(sum);
         
+        // Input in Java
+
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next(); // next() --> stores single word input
+        // System.out.println(input);
+
+        // String fullName = sc.nextLine(); //nextLine() --> stores value with spaces
+        // System.out.println(fullName);
+
+        int number = sc.nextInt(); // stores integer input
+        System.out.println(number);
     }
 }
