3aba3cc3af0a5770cfd13bfb31b657a3184c844f
Amit Saha <asaha.bwn2000@gmail.com>
2023-06-30 20:23:06 +0530

    learning java

--- conditionalStatement.java
+++ conditionalStatement.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class conditionalStatement {
     public static void main(String[] args) {
-        int age  = 16;
+        int age  = 15;
         if (age>=18){
             System.out.println("Adult");
         }
@@ -13,5 +13,13 @@ public class conditionalStatement {
         else{
             System.out.println("Child");
         }
+
+        // Ternary operator
+
+        // syntax --> variable = condition ? statement1 : statement2
+        // if condition is true statment1 will be executed, otherwise statement2 will be executed
+
+        String type = (age % 2 == 0) ? "Even" : "Odd";
+        System.out.println(type);
     }
 }
