c9bffd48847830f208fdc808c602a1299e8e7668
Amit Saha <asaha.bwn2000@gmail.com>
2023-07-05 20:45:58 +0530

    Loops in java

---
+++ loops.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class loops {
+    public static void main(String[] args) {
+        
+        // while loop
+
+        int counter = 1;
+        while (counter <= 10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
