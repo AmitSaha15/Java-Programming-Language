fa16ed34ebae20c0eddceced01cb8af25c4202a3
Amit Saha <asaha.bwn2000@gmail.com>
2023-07-17 20:07:57 +0530

    Printing inverted star pattern

--- invertedStarPattern.java
+++ invertedStarPattern.java
@@ -1,10 +1,7 @@
-import java.util.*;
 
 public class invertedStarPattern {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of stars in first row: ");
-        int n = sc.nextInt();
+        int n = 4;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j<= n-i+1; j++) {
                 System.out.print("*");
