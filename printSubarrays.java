f7f3b0edfd617e947998e6048fe8a209189c0bf4
Amit Saha <asaha.bwn2000@gmail.com>
2023-08-12 23:29:51 +0530

    Print subarrays

---
+++ printSubarrays.java
@@ -0,0 +1,23 @@
+public class printSubarrays {
+
+    public static void print_subarrays(int numbers[]){
+        int ts = 0;
+        for(int i = 0; i < numbers.length; i++){
+            int start = i;
+            for(int j = i; j < numbers.length; j++){
+                int end = j;
+                for(int k = start; k <= end; k++){
+                    System.out.print(numbers[k] + " "); //subarray
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarrays = " + ts);
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        print_subarrays(numbers);
+    }
+}
