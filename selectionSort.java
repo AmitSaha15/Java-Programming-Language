23ececf547eac19cc5a662ee9b9f8e5f13f22be8
Amit Saha <asaha.bwn2000@gmail.com>
2023-09-04 01:22:44 +0530

    Selection sort code

--- selectionSort.java
+++ selectionSort.java
@@ -4,7 +4,8 @@ public class selectionSort {
         for(int i=0; i<arr.length-1; i++){
             int minPos = i;
             for(int j=i+1; j<arr.length;j++){
-                if(arr[minPos] > arr[j]){
+                if(arr[minPos] > arr[j]){ //condition for increasing order
+                    // for decreasing order --> condition: arr[minPos] < arr[j]
                     minPos = j;
                 }
             }
